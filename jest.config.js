88508db31c02c54fa9f8cc9f6becc09be79959de
Ankur Sinha <sinhaankur@ymail.com>
2026-04-03 11:26:27 -0400

    test: ignore build and nested repo paths in jest

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 export default {
   testEnvironment: 'jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom'],
+  modulePathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/super-octo-rotary-phone/'],
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/super-octo-rotary-phone/'],
+  watchPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/super-octo-rotary-phone/'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
   },
